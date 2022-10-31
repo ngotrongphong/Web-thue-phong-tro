048e9c901d8a03806612bcb43c661f748201d0b1
phongngotrong <ngotrongphong.dev@gmail.com>
2022-10-31 22:40:59 +0700

    fix nav

--- client/src/containers/Public/Navigation.js
+++ client/src/containers/Public/Navigation.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navigation = () => {
   return (
     <div className="w-screen flex justify-center items-center h-[40px] bg-secondary text-white">
-      <div className="w-1100 ">Nav</div>
+      <div className="w-1100 ">Chỗ này là thanh nav</div>
     </div>
   );
 };
