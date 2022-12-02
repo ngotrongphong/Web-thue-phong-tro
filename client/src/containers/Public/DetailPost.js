006f20d5f4aeff598e037a5dc13ad960491be12c
phongngotrong <ngotrongphong.dev@gmail.com>
2022-12-02 17:23:44 +0700

    slick slide

--- client/src/containers/Public/DetailPost.js
+++ client/src/containers/Public/DetailPost.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getPostsLimit } from "../../store/actions";
+import { Slider } from "../../components";
 
 const DetailPost = () => {
   const { postId } = useParams();
   const dispatch = useDispatch();
+  const { posts } = useSelector((state) => state.post);
 
   useEffect(() => {
     postId &&
@@ -15,7 +17,18 @@ const DetailPost = () => {
         })
       );
   }, [postId]);
-  return <div>DetailPost</div>;
+  return (
+    <div className="flex w-full gap-4">
+      <div className="w-[70%]">
+        <Slider
+          images={
+            posts && posts.length > 0 && JSON.parse(posts[0]?.images?.image)
+          }
+        />
+      </div>
+      <div className="w-[30%]"></div>
+    </div>
+  );
 };
 
 export default DetailPost;
